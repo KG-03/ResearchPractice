92c392ab9789f5c2d483db55e2d3b84f7f05b83d
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-01-28 22:29:46 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    long long A = 0;
+    long long B = 0;
+    long long C = 0;
+
+    cin >> A;
+    cin >> B;
+    cin >> C;
+
+    cout << A + B + C << endl;
+}
