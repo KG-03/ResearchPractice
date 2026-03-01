42309be6a69c5b6c4b3b0ca6223b09fe46f60e60
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-03-01 11:10:51 +0900

    Create script.js
    자바스크립트 맛보기

---
+++ HTML/March/script.js
@@ -0,0 +1,21 @@
+"use strict";
+console.log("Hello March");
+
+let message = "3월 시작";
+console.log(message);
+
+message = "자바스크립트 시작";
+console.log(message);
+
+let number = 10;
+number = number + 5;
+console.log(number);
+
+/* 1일차
+ * let      : 변수 선언.
+ * const    : 상수 선언.
+ * 
+ * "use strict";    : 자바 스크립트를 더 엄격하고 안전하게 실행하라는 선언.
+ *                    최근에는 기본적으로 strict 모드가 적용된다고는 하지만,
+ *                    배우는 단계라서 적용중.
+ */
