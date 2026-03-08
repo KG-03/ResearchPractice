c6cfaab42383a036670559547ef85b15a7ab6f38
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-03-08 10:28:34 +0900

    Create script_week2.js
    2주차 js 파일
    if문 확인.

---
+++ HTML/March/script_week2.js
@@ -0,0 +1,27 @@
+"use strict";
+
+//if문
+let age_1 = 20;
+if (age_1 >= 18) {
+    console.log("age_1은 성인입니다.");
+}
+
+let age_2 = 15;
+if(age_2 >= 18) {
+    console.log("age_2는 성인입니다.");
+}
+
+//아래의 number_1을 3, 7, 5로 변경해가며 확인.
+let number_1 = 5;
+if(number_1 > 5) {
+    console.log("number_1은 5보다 큽니다.");
+}
+
+let x_1 = 10;
+if(x_1 === "10") {
+    console.log("x_1은 \"10\"과 같습니다.");
+}
+
+/* 8일차
+ * if (조건문) {실행문} : if문 구조
+ */
