17b35279b340984f8749cea8e3e63323f5d4b7e8
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-03-23 11:03:49 +0900

    Update script_week4.js
    document.querySelector
    document.querySelectorAll
    id 값, class 값 불러내기
    불러낸 값을 변경해서 전달하기

--- HTML/March/script_week4.js
+++ HTML/March/script_week4.js
@@ -13,6 +13,34 @@ console.log(h2.textContent);
 let div = document.querySelector("div");
 console.log(div.textContent);
 
+let intro = document.querySelector("#intro");
+console.log(intro.textContent);
+
+let weekCards = document.querySelector(".week-card");
+console.log(weekCards.textContent);
+
+weekCards = document.querySelectorAll(".week-card");
+console.log(weekCards);
+for (let i = 0; i < weekCards.length; i++) {
+    console.log(weekCards[i].textContent);
+}
+
+/* 아래의 내용부터는 html에서 존재하지 않는 값을 호출하기 때문에 각주 처리.
+let main = document.querySelector("#main");
+console.log(main.textContent);
+
+let items = document.querySelectorAll(".item");
+for (let i = 0; i < items.length; i++) {
+    console.log(items[i].textContent);
+}
+
+let boxs = document.querySelectorAll(".box");
+for (let i = 0; i < boxs.length; i++) {
+    boxs[i].textContent = boxs[i].textContent * 2;
+}
+*/
+
+
 
 /* 22일차
  * let title = document.querySelector("h1");
@@ -21,4 +49,10 @@ console.log(div.textContent);
  *             이 방법으로 값을 가져오면 '첫 번째 요소'를 가져온다.
  * 
  * textContent  : HTML 전체가 아닌, 내용만 가져오도록 하는 방법.
+ */
+
+/* 23일차
+ * querySelector    : 1개
+ * querySelectorAll : 여러 개
+ *                    여기서 호출된 값은 여기서 값을 바꿔서 변경할 수 있다.
  */
