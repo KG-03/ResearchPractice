3ee5bc8f83cfc838aebd6f08906b812b907dc4ac
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-03-25 11:40:04 +0900

    Update script_week4.js
    버튼, 이벤트 넣는 방법.

--- HTML/March/script_week4.js
+++ HTML/March/script_week4.js
@@ -61,6 +61,38 @@ for (let i = 0; i < boxs.length; i++) {
 }
 */
 
+let btn = document.querySelector(".intro button");
+btn.addEventListener("click", function() {
+    console.log("클릭됨!");
+    introP[3].textContent = "바뀜!";
+});
+
+/* 아래의 내용부터는 html에서 존재하지 않는 값을 호출하기 때문에 각주 처리.
+let btn = document.querySelector("#btn");
+let p = document.querySelector("#msg");
+btn.addEventListener("click", function(){
+    p.textContent = "반가워!";
+});
+
+let btn = document.querySelector("#btn");
+let p = document.querySelector("#num");
+btn.addEventListener("click", function(){
+    p.textContent = Number(p.textContent) + 1;
+});
+
+let btn = document.querySelector("#btn");
+let p = document.querySelector("#text");
+btn.addEventListener("click", function(){
+    if(p.textContent === "ON") p.textContent = "OFF";
+    else p.textContent = "ON";
+});
+
+위의 3번 문제의 조건문은 다음과 같이 요약 가능.
+p.textContent = p.textContent === "ON" ? "OFF" : "ON";
+
+문제 풀이였기 때문에 모든 변수명을 btn, p 등으로 일괄처리 했으나, 실전에서는 이러한 형식을 쓰면 안 된다는 것을 인지하고 있음.
+*/
+
 
 
 /* 22일차
@@ -83,4 +115,12 @@ for (let i = 0; i < boxs.length; i++) {
  *                  id, class 안에 있는 p를 불러오기 위해서 쓰는 방법.
  * 
  * 불러낸 값 안에 다른 값을 집어넣는 게 가능.
+ */
+
+/* 25일차
+ * let btn = document.querySelector("#btn");
+ * btn.addEventListener("click", function() { ... })
+ *      위의 방식은 버튼에 이벤트 넣는 방법.
+ *      만일, addEventListener("click", console.log(...) ) 이와 같은 형식으로 코드를 작성하면, 즉시 실행되어 문제가 생김.
+ *              따라서, 이벤트를 넣을 때는 function()을 이용해야 함.
  */
