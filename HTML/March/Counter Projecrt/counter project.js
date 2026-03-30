73000f12d465dce8ccd8e05827a4ade6ce92a91f
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-03-30 11:44:21 +0900

    Update counter project.js
    카운트 숫자 제한,
    리셋,
    카운트 숫자 상태를 색으로 표시

--- HTML/March/Counter Projecrt/counter project.js
+++ HTML/March/Counter Projecrt/counter project.js
@@ -2,15 +2,37 @@
 
 let plusBtn = document.querySelector("#plus");
 let minusBtn = document.querySelector("#minus");
+let resetBtn = document.querySelector("#reset");
 let count = document.querySelector("#num")
 let num = 0;
 
 plusBtn.addEventListener("click", function() {
-    num++;
-    count.textContent = num;
+    if (num < 100){
+        num++;
+        update();
+    }
 })
 
 minusBtn.addEventListener("click", function() {
-    num--;
-    count.textContent = num;
+    if(num > -100) {
+        num--;
+        update();
+    }
 })
+
+resetBtn.addEventListener("click", function(){
+    num = 0;
+    update();
+})
+
+function update() {
+    count.textContent = num;
+
+    if (num > 0) {
+        count.style.color = "green";
+    } else if (num < 0) {
+        count.style.color = "red";
+    } else {
+        count.style.color = "black";
+    }
+}
