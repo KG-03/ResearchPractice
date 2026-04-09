878bf1e38c7f496ef18421d9fec673d3f4825074
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-04-09 12:06:12 +0900

    Update ToDo-List.js
    엔터 버튼으로도 값 입력 가능하게 작성
    전체삭제 버튼 작동 코드 작성
    
    리스트 추가 함수를 따로 생성하여 관리

--- HTML/April/ToDo-List/ToDo-List.js
+++ HTML/April/ToDo-List/ToDo-List.js
@@ -1,10 +1,27 @@
 let input = document.querySelector(".input");
 let btn = document.querySelector(".btn");
+let delall = document.querySelector(".delall");
 let list = document.querySelector(".list");
 let guide = document.querySelector(".guide");
 
 btn.addEventListener("click", function() {
+    addList();
+});
+
+delall.addEventListener("click", function() {
+    list.innerHTML = "";
+    updateGuide();
+});
+
+input.addEventListener("keydown", function(e) {
+    if (e.key === "Enter") {
+        addList();
+    }
+});
+
+function addList() {
     let text = input.value.trim();
+
     if(text === "") {
         guide.textContent = "입력해 주십시오.";
         return;
@@ -20,6 +37,7 @@ btn.addEventListener("click", function() {
     delbtn.classList.add("d-btn");
     delbtn.addEventListener("click", function() {
         li.remove();
+        updateGuide();
     })
 
     let donebtn = document.createElement("button");
@@ -30,8 +48,26 @@ btn.addEventListener("click", function() {
 
     li.appendChild(delbtn);
     li.appendChild(donebtn);
-
-
     list.appendChild(li);
+    
+    updateGuide();
     input.value = "";
-})
+    input.focus();
+};
+
+function updateGuide() {
+    if (list.children.length) {
+        guide.textContent = list.children.length + "개의 할 일이 있습니다.";
+        return;
+    }
+
+    guide.textContent = "";
+}
+
+/* 8일차
+ * todolist 설계
+ */
+
+/* 9일차
+ * 편의성 추가 (기능확장, ux 개선)
+ */
