d75933ce69ae3348b0befcf42763edb51ea061f4
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-04-28 12:42:22 +0900

    Update 좌표 정렬하기 2.cc
    이상한 결과가 업로드되어 수정.

--- C++17/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
+++ C++17/백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.cc
@@ -1,12 +1,31 @@
-<html>
-<head><title>504 Gateway Time-out</title></head>
-<body>
-<center><h1>504 Gateway Time-out</h1></center>
-</body>
-</html>
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
-<!-- a padding to disable MSIE and Chrome friendly error page -->
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    
+    int n = 0;
+    cin >> n;
+
+    vector<pair<int, int>>vec;
+    int x = 0;
+    int y = 0;
+
+    for(int i = 0; i < n; i++) {
+        cin >> x;
+        cin >> y;
+        vec.push_back(pair<int, int>(y, x));
+    }
+
+    sort(vec.begin(), vec.end());
+
+    for(int i = 0; i < n; i++) {
+        cout << vec[i].second << " " << vec[i].first << "\n";
+    }
+    
+    return 0;
+}
