f0fb3e83d05c377bda01124a0de568985b0fd49b
KG-03 <129266219+KG-03@users.noreply.github.com>
2026-05-03 12:57:19 +0900

    Update Memo-App.js
    삭제 기능 구현.
    addEventListener("click", deleteNote(note.id))
    해당 코드가 '즉시 실행'인 것을 확인했다.

--- HTML/May/Memo-App/Memo-App.js
+++ HTML/May/Memo-App/Memo-App.js
@@ -11,7 +11,7 @@ function addNote() {
     const title = titleInput.value.trim();
     const content = contentInput.value.trim();
 
-    //비어있으면 생략
+    //비어있으면
     if (!title && !content) return;
 
     const note = {
@@ -29,8 +29,18 @@ function addNote() {
     console.log("addNote");
 }
 
+function deleteNote(id) {
+    notes = notes.filter(note => note.id !== id);
+    renderNotes();
+}
+
 function renderNotes() {
     noteList.innerHTML = "";
+
+    if(!notes.length) {
+        noteList.innerHTML = "<p>메모가 없습니다.</p>";
+        return;
+    }
 
     notes.forEach(note => {
         const card = document.createElement("div");
@@ -39,8 +49,22 @@ function renderNotes() {
         card.innerHTML = `
             <h3>${note.title || "(제목 없음)"}</h3>
             <p>${note.content || "(내용 없음)"}</p>
+            <button class="delete-btn">삭제</button>
         `;
+
+        const delBtn = card.querySelector(".delete-btn");
+        delBtn.addEventListener("click", () => deleteNote(note.id));
 
         noteList.appendChild(card);
     });
 }
+
+/* 1일차
+ * 구조 생성
+ */
+
+/* 3일차
+ * delBtn.addEventListener("click", deleteNote(note.id));   : 이 형식은 '즉시 실행'으로 이어진다.
+ *                                                            이벤트 형식으로 할 거면 "click", fucntion() {...}으로 이어져야만 한다.
+ *                                                            혹은 () => deleteNote(note.id)도 괜찮다.
+ */
